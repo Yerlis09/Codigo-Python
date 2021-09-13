bb1ea8862bd7c428eaa58ea5f017e45a2e5930af
Djbermor <deividber@hotmail.com>
2021-09-12 21:41:44 -0500

    creacion de la funcionalidad que permite retornar el codigo ASCII asociado a un entero

--- ultimo_digito.py
+++ ultimo_digito.py
@@ -11,7 +11,7 @@ print('El ultimo digito es: %i' %resultado)
 def retornar_ultimo_dos_digitos(numero):
     if(numero == None):
         return 'Ingrese un valor valido, por favor'
-    elif(numero == ""):
+    elif(numero == ""): 
         return 'ingrese un valor por favor'
     elif (numero > -100) and (numero < 100):
         return 'Ingrese un valor menor a -100 y mayor a 100'
@@ -42,9 +42,11 @@ print(resultado)
 
 def retornar_cantidad_de_digitos_pares(numero):
     if (numero == None):
-        return 'Ingrese un valor valido, por favor'
-    elif (numero == '') and (numero < 0):
-        return 'Ingrese un valor o un numero entero positivo'
+        return 'Ingrese un valor valido'
+    elif (numero == ''):
+        return 'Ingrese un valor' 
+    elif (numero < 0):
+        return 'Ingrese un numero entero positivo'
     elif (numero == 1):
         return 0
     else:
@@ -54,7 +56,52 @@ def retornar_cantidad_de_digitos_pares(numero):
                 cont +=1
         return cont
 
-resultado = retornar_cantidad_de_digitos_pares(2)
+resultado = retornar_cantidad_de_digitos_pares('')
 print(resultado)
 
-    
+# Construir una funcion que reciba como parametro un entero y retorne la cantidad de digitos primos
+
+def retornar_si_es_primo(numero): # Funcion que permite verificar si el número ingresado es primo o no
+    if (numero > 1):
+        for i in range(2,numero):
+            if( numero % i == 0):
+                return False      
+        return True
+
+def retornar_cantidad_digitos_primos(numero): # Funcion que permite hallar la cantidad de números primo
+    if (numero == None):
+        return 'Ingrese un valor valido'
+    elif (numero == ''):
+        return 'Ingrese un valor' 
+    elif (numero < 0):
+        return 'Ingrese un numero entero positivo'
+    elif (numero == 1) :
+        return 0
+    if (numero == 2):
+        return 1
+    count = 0
+    for i in range(2, (numero + 1)):
+        if (retornar_si_es_primo(i) == True):
+            count += 1
+    return count
+
+resultado = retornar_cantidad_digitos_primos(10)
+print(resultado)
+
+# Construir una funcion que reciba como parametro un entero y retorne el codigo ASCII asociado a el
+
+def retornar_codigo_ascii_del_entero(entero):
+    if (entero == None):
+        return('Ingrese un valor valido, por favor')
+    elif (entero == ''):
+        return 'Ingrese un valor, por favor'
+    elif (type(entero) == str):
+        return 'Ingrese un entero positivo, por favor'
+    elif (entero < 0):
+        return 'Ingrese un valor superior a -1, por favor'
+    else:
+        codigo_ascii = ord(str(entero))
+        return codigo_ascii
+
+resultado = retornar_codigo_ascii_del_entero(7)
+print(resultado)
